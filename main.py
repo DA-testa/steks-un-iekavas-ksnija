ae67983787b667a53a5b3927504c77da0d72ca0e
ksnija <125401025+ksnija@users.noreply.github.com>
2023-02-18 21:45:43 +0000

    1

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def find_mismatch(text):
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
     else: 
-         return "Success"
+        return "Success"
 
 
 def main():
