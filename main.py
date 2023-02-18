da45868cb78d37709955c5f85c75ed65170d24c4
ksnija <125401025+ksnija@users.noreply.github.com>
2023-02-18 19:19:30 +0000

    ffail

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[len(opening_brackets_stack)-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
             
             
      if opening_brackets_stack:
-        return opening_brackets_stack[0].position
+        return opening_brackets_stack[-1].position
      else: 
          return "Success"
 
@@ -41,5 +41,5 @@ def main():
         pass
     else:
         print("Jūsu tekstam jābūt I vai F burtam")
-        
+
     main()
