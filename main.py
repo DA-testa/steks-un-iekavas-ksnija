ea884be1c8dd15f09ea0fcbb3203cf2ab605e4a2
ksnija <125401025+ksnija@users.noreply.github.com>
2023-02-18 22:00:21 +0000

    1

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def find_mismatch(text):
             # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
-        opening_brackets_stack.pop()    
+            opening_brackets_stack.pop()    
             
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
@@ -39,4 +39,4 @@ def main():
     else:
         print("Jūsu tekstam jābūt I vai F burtam")
 
-    main()
+main()
