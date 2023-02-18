a0647617de43c438adfe65c62c63985c1ae1ce99
ksnija <125401025+ksnija@users.noreply.github.com>
2023-02-18 21:39:26 +0000

    check2

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ def find_mismatch(text):
             # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
-            opening_brackets_stack.pop()
-            
+             opening_brackets_stack.pop()    
             
      if opening_brackets_stack:
         return opening_brackets_stack[0].position
